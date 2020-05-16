8c7aec823621531190187685c895ac415ebea7d2
Dan Marcus <dan.r.marcus@gmail.com>
2020-05-16 00:27:20 -0400

    Stats

--- stats.inc.php
+++ stats.inc.php
@@ -52,44 +52,72 @@ $stats_type = array(
     // Statistics global to table
     "table" => array(
 
+        /*
         "turns_number" => array("id"=> 10,
                     "name" => totranslate("Number of turns"),
                     "type" => "int" ),
+        */
 
-/*
-        Examples:
-
-
-        "table_teststat1" => array(   "id"=> 10,
-                                "name" => totranslate("table test stat 1"), 
-                                "type" => "int" ),
-                                
-        "table_teststat2" => array(   "id"=> 11,
-                                "name" => totranslate("table test stat 2"), 
-                                "type" => "float" )
-*/  
     ),
     
     // Statistics existing for each player
     "player" => array(
 
-        "turns_number" => array("id"=> 10,
-                    "name" => totranslate("Number of turns"),
+        "actions_taken" => array("id"=> 10,
+                    "name" => totranslate("Number of actions taken"),
                     "type" => "int" ),
-    
-/*
-        Examples:    
-        
-        
-        "player_teststat1" => array(   "id"=> 10,
-                                "name" => totranslate("player test stat 1"), 
-                                "type" => "int" ),
-                                
-        "player_teststat2" => array(   "id"=> 11,
-                                "name" => totranslate("player test stat 2"), 
-                                "type" => "float" )
+        "points_aristocrats_end" => array("id"=> 11,
+                    "name" => totranslate("Points from Aristocrat types"),
+                    "type" => "int" ),
+        "points_rubles_end" => array("id"=> 12,
+                    "name" => totranslate("Points from Rubles"),
+                    "type" => "int" ),
+        "points_hand_end" => array("id"=> 13,
+                    "name" => totranslate("Points from cards in hand"),
+                    "type" => "int" ),
+        "points_total" => array("id"=> 14,
+                    "name" => totranslate("Points from played cards"),
+                    "type" => "int" ),
+        "points_Worker" => array("id"=> 15,
+                    "name" => totranslate("Points from Workers"),
+                    "type" => "int" ),
+        "points_Building" => array("id"=> 16,
+                    "name" => totranslate("Points from Buildings"),
+                    "type" => "int" ),
+        "points_Aristocrat" => array("id"=> 17,
+                    "name" => totranslate("Points from Aristocrats"),
+                    "type" => "int" ),
+        "pub_points" => array("id"=> 18,
+                    "name" => totranslate("Points bought with Pub"),
+                    "type" => "int" ),
+        "rubles_total" => array("id"=> 19,
+                    "name" => totranslate("Rubles from played cards"),
+                    "type" => "int" ),
+        "rubles_Worker" => array("id"=> 20,
+                    "name" => totranslate("Rubles from Workers"),
+                    "type" => "int" ),
+        "rubles_Building" => array("id"=> 21,
+                    "name" => totranslate("Rubles from Buildings"),
+                    "type" => "int" ),
+        "rubles_Aristocrat" => array("id"=> 22,
+                    "name" => totranslate("Rubles from Aristocrats"),
+                    "type" => "int" ),
+        "rubles_spent" => array("id"=> 23,
+                    "name" => totranslate("Rubles spent"),
+                    "type" => "int" ),
+        "cards_bought" => array("id"=> 24,
+                    "name" => totranslate("Cards bought"),
+                    "type" => "int" ),
+        "cards_traded" => array("id"=> 25,
+                    "name" => totranslate("Trading cards bought"),
+                    "type" => "int" ),
+        "cards_added" => array("id"=> 26,
+                    "name" => totranslate("Cards added to hand"),
+                    "type" => "int" ),
+        "observatory_draws" => array("id"=> 27,
+                    "name" => totranslate("Cards drawn with Observatory"),
+                    "type" => "int" ),
 
-*/    
     )
 
 );
