38522cae3542c6c7afb051aece31fddc96aca66a
Dan Marcus <dan.r.marcus@gmail.com>
2020-05-18 15:20:50 -0400

    Remove old unused db table

--- dbmodel.sql
+++ dbmodel.sql
@@ -33,13 +33,6 @@
 -- Example 2: add a custom field to the standard "player" table
 -- ALTER TABLE `player` ADD `player_my_custom_field` INT UNSIGNED NOT NULL DEFAULT '0';
 
-CREATE TABLE IF NOT EXISTS `board` (
-  `board_x` smallint(5) unsigned NOT NULL,
-  `board_y` smallint(5) unsigned NOT NULL,
-  `board_player` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`board_x`,`board_y`)
-) ENGINE=InnoDB;
-
 CREATE TABLE IF NOT EXISTS `card` (
   `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `card_type` varchar(16) NOT NULL,
